a24993c66fa3329f5cef7e5c328e101439c681f6
Dange <SHIVAM.DANGE@finastra.com>
2020-02-12 14:37:39 +0530

    my first commit

---
+++ src/com/finastra/Main.java
@@ -0,0 +1,8 @@
+package com.finastra;
+
+public class Main {
+    public static void main(String[] args) {
+
+        
+    }
+}
---
+++ src/com/finastra/Sum.java
@@ -0,0 +1,7 @@
+package com.finastra;
+
+public class Sum {
+    static int  add(int a,int b){
+        return a+b;
+    }
+}
